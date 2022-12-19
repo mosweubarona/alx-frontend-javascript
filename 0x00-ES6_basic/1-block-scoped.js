21e469e128fec6c48ac053eb33132f76079fb098
mosweubarona <mosweubarona@gmail.com>
2022-12-19 14:21:23 -0800

    m correct syntax error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    /* cont make varialbles unchanged */
+    /* eslint--disable-next-line*/
     const task = true;
     const task2 = false;
+    /* eslint-disable-next-line */
   }
 
   return [task, task2];
