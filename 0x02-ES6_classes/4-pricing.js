e168ec05ff70f11d0225a1d1d48f8feba7a2c33d
mosweubarona <mosweubarona@gmail.com>
2022-12-22 19:21:13 +0000

    m fix return amount

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,22 +1,37 @@
+import Currency from './3-currency';
+
 export default class Pricing {
   constructor(amount, currency) {
-    (this.amount = amount), (this.currency = currency);
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = value;
-  }
-
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = value;
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+    this._currency = currency;
   }
 
   displayFullPrice() {
@@ -24,6 +39,12 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+
     return amount * conversionRate;
   }
 }
