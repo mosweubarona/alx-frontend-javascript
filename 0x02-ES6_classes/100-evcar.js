f740b1156a84a6746e16d566fb9f40dcd52ada6e
mosweubarona <mosweubarona@gmail.com>
2022-12-22 19:32:29 +0000

    m fix adv

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,13 @@
 import Car from './10-car';
 
-class EVCar extends Car {
-  constructor(brand = '', motor = '', color = '', range = '') {
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  // eslint-disable-next-line
+	cloneCar() {
+    return new Car[Symbol.species]();
   }
 }
-
-export default EVCar;
