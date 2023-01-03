f5b0f6bfed3eb857d4f4667ecaa9d4d2c370ca95
mosweubarona <mosweubarona@gmail.com>
2023-01-03 21:10:46 +0000

    m fixed lower part adv returnning instance o car

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,12 @@
-import Car from './10-car';
+import Car from "./10-car";
 
-class EVCar extends Car {
-  constructor(brand = '', motor = '', color = '', range = '') {
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  cloneCar() {
+    return new Car();
   }
 }
-
-export default EVCar;
