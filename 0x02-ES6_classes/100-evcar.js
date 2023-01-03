b5c77e3e482d1227085ff2e6a58f054d7019eb2a
mosweubarona <mosweubarona@gmail.com>
2023-01-03 21:19:13 +0000

    m fix eslint

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,4 +1,4 @@
-import Car from "./10-car";
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
@@ -6,7 +6,8 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  cloneCar() {
+  // eslint-disable-next-line
+	cloneCar() {
     return new Car();
   }
 }
